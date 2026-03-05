364dd8c89006feacc359f7636a485360278aeee9
Khong Dan <143010149+Khongdan@users.noreply.github.com>
2026-03-05 10:26:34 +0700

    Delete Điện toán Di động 2 directory

--- Điện toán Di động 2/src/client/ClientApp.java
+++
@@ -1,8 +0,0 @@
-package client;
-import javax.swing.SwingUtilities;
-public class ClientApp {
-    public static void main(String[] args) {
-        // Khởi chạy giao diện Client trên luồng sự kiện Swing
-        SwingUtilities.invokeLater(() -> new ClientGUI().setVisible(true));
-    }
-}
