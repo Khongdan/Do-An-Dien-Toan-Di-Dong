70e6ef415928290ee0788ef0a6785e92d48485bd
Khong Dan <143010149+Khongdan@users.noreply.github.com>
2026-03-05 10:18:45 +0700

    Delete Điện toán Di động 2 directory

--- Điện toán Di động 2/src/server/ServerApp.java
+++
@@ -1,8 +0,0 @@
-package server;
-import javax.swing.SwingUtilities;
-public class ServerApp {
-    public static void main(String[] args) {
-        // Khởi chạy giao diện Server trên luồng sự kiện Swing
-        SwingUtilities.invokeLater(() -> new ServerGUI().setVisible(true));
-    }
-}
